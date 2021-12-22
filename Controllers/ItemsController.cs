e2310489e0b68703a6d2371be35938c70adbbf9f
mark <markpackham1@gmail.com>
2021-12-22 11:20:26 +0000

    add Read and Show functionality
    https://youtu.be/ZXdFisA_hOY?t=2471

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,10 +1,41 @@
+using System.Collections.Generic;
+using CSharpCRUDCatalog.Entities;
+using CSharpCRUDCatalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CSharpCRUDCatalog.Controllers
 {
+
     [ApiController]
+    [Route("items")]
     public class ItemsController : ControllerBase
     {
+         private readonly InMemItemsRepository repository;
 
+         public ItemsController(){
+             repository = new InMemItemsRepository();
+         }
+
+        // GET /items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+
+        // GET /items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
     }
 }
