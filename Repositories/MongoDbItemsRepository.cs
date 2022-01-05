1bfd996ffb1af6b90fe2bfa0d99f1d0240561bb7
mark <markpackham1@gmail.com>
2022-01-05 16:37:11 +0000

    implement interface for mongodb repository

--- Repositories/MongoDbItemsRepository.cs
+++ Repositories/MongoDbItemsRepository.cs
@@ -1,6 +1,32 @@
+using CSharpCRUDCatalog.Entities;
+
 namespace CSharpCRUDCatalog.Repositories
 {
     public class MongoDbItemsRepository : IInMemItemsRepository
     {
+        public void CreateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
