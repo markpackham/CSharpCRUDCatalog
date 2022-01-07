f2a731e81fb197ea970cf4ffdae0c7e0a0471337
mark <markpackham1@gmail.com>
2022-01-07 14:43:11 +0000

    add MongoDB specific settings
    https://youtu.be/ZXdFisA_hOY?t=6176

--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -2,5 +2,9 @@
 {
     public class MongoDbSettings
     {
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+        public string ConnectionString { get { return $"mongodb://{Host}:{Port}"; } }
     }
 }
