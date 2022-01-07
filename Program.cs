17b6ff8f0b10af70c8ed70a50645e246a978d703
mark <markpackham1@gmail.com>
2022-01-07 14:58:34 +0000

    add MongoDB client service
    https://youtu.be/ZXdFisA_hOY?t=6366

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
 using CSharpCRUDCatalog.Repositories;
+using CSharpCRUDCatalog.Settings;
+using MongoDB.Driver;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IMongoClient>(serviceProvier =>
+{
+    var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+    return new MongoClient(settings.ConnectionString);
+});
 builder.Services.AddSingleton<IInMemItemsRepository, InMemItemsRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,7 +18,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+ 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
