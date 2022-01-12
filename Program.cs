3fd087d74c592a3ae3a1f24d124a12c8ca6316f0
mark <markpackham1@gmail.com>
2022-01-12 14:46:57 +0000

    prevent .Net from removing Async suffix at runtime
    https://youtu.be/ZXdFisA_hOY?t=8673

--- Program.cs
+++ Program.cs
@@ -17,7 +17,10 @@ builder.Services.AddSingleton<IMongoClient>(serviceProvier =>
     return new MongoClient(settings.ConnectionString);
 });
 builder.Services.AddSingleton<IInMemItemsRepository, MongoDbItemsRepository>();
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.SuppressAsyncSuffixInActionNames = false;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
