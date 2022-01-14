72e93d0549150af53cbf3bff4067a7f8a62b2ea5
mark <markpackham1@gmail.com>
2022-01-14 17:17:12 +0000

    add more detailed health check response
    https://youtu.be/ZXdFisA_hOY?t=10824

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 using CSharpCRUDCatalog.Repositories;
 using CSharpCRUDCatalog.Settings;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
+using System.Net.Mime;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
@@ -25,7 +28,7 @@ builder.Services.AddControllers(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks()
-    .AddMongoDb(mongoDbSettings.ConnectionString, name: "mongodb", timeout: TimeSpan.FromSeconds(5));
+    .AddMongoDb(mongoDbSettings.ConnectionString, name: "mongodb", timeout: TimeSpan.FromSeconds(5), tags: new[] { "ready" });
 
 var app = builder.Build();
  
@@ -48,7 +51,35 @@ app.MapControllers();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
-    endpoints.MapHealthChecks("/healthcheck");
+    endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+    {
+        // make sure Database is ready to serve requests
+        Predicate = (check) => check.Tags.Contains("ready"),
+        ResponseWriter = async(context, report) =>
+        {
+            var result = JsonSerializer.Serialize(
+                new
+                {
+                    status = report.Status.ToString(),
+                    checks = report.Entries.Select(entry => new
+                    {
+                        name = entry.Key,
+                        status = entry.Value.Status.ToString(),
+                        exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                        duration = entry.Value.Duration.ToString(),
+                    })
+                }
+                );
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+            await context.Response.WriteAsync(result);
+        }
+    });
+
+    endpoints.MapHealthChecks("/health/live", new HealthCheckOptions
+    {
+        // make sure site service is up and running
+        Predicate = (_) => false
+    });
 }
 );
 
